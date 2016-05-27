8b02af4650c7e834a0b86a9167660e4602950114
Tracy Levesque <tracy@yikesinc.com>
2016-05-27 17:15:48 -0400

    Added an assets folder and boilerplate files generated by https://github.com/Tmeister/wppb-gen-site Started editing the readme.txt

---
+++ includes/class-yikes-inc-level-playing-field-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://www.yikesinc.com
+ * @since      1.0.0
+ *
+ * @package    Yikes_Inc_Level_Playing_Field
+ * @subpackage Yikes_Inc_Level_Playing_Field/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Yikes_Inc_Level_Playing_Field
+ * @subpackage Yikes_Inc_Level_Playing_Field/includes
+ * @author     YIKES, Inc. <plugins@yikesinc.com>
+ */
+class Yikes_Inc_Level_Playing_Field_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
