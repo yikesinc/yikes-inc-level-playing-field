f176e896f3114e9597c7b94965c59412a565081b
Jeremy Pry <jeremy.pry@gmail.com>
2017-10-04 16:49:27 -0400

    Initial Application Manager repository

---
+++ new/src/Model/ApplicationManagerRepository.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * YIKES Inc. Level Playing Field Plugin.
+ *
+ * @package Yikes\LevelPlayingField
+ * @author  Jeremy Pry
+ * @license GPL2
+ */
+
+namespace Yikes\LevelPlayingField\Model;
+
+use Yikes\LevelPlayingField\CustomPostType\ApplicationManager as ApplicationManagerCPT;
+use Yikes\LevelPlayingField\Exception\InvalidPostID;
+
+/**
+ * Class ApplicationManagerRepository
+ *
+ * @since   %VERSION%
+ * @package Yikes\LevelPlayingField
+ */
+class ApplicationManagerRepository extends CustomPostTypeRepository {
+
+	/**
+	 * Find the Talk with a given post ID.
+	 *
+	 * @since %VERSION%
+	 *
+	 * @param int $id Post ID to retrieve.
+	 *
+	 * @return ApplicationManager
+	 * @throws InvalidPostID If the post for the requested ID was not found.
+	 */
+	public function find( $id ) {
+		$post = get_post( $id );
+		if ( null === $post ) {
+			throw InvalidPostID::from_id( $id );
+		}
+
+		return new ApplicationManager( $post );
+	}
+
+	/**
+	 * Find all the published Talks.
+	 *
+	 * @since %VERSION%
+	 *
+	 * @return ApplicationManager[]
+	 */
+	public function find_all() {
+		$args  = array(
+			'post_type'   => ApplicationManagerCPT::SLUG,
+			'post_status' => array( 'active', 'inactive' ),
+		);
+		$query = new \WP_Query( $args );
+
+		$jobs = array();
+		foreach ( $query->posts as $post ) {
+			$jobs[ $post->ID ] = new ApplicationManager( $post );
+		}
+
+		return $jobs;
+	}
+}
