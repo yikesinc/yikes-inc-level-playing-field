1a20ac86a4a5727fadae0c0b07d6766bab3b5940
Jeremy Pry <jeremy.pry@gmail.com>
2017-10-04 13:52:11 -0400

    Create initial meta models

---
+++ new/src/Model/ApplicantManagerMeta.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * YIKES Inc. Level Playing Field Plugin.
+ *
+ * @package Yikes\LevelPlayingField
+ * @author  Jeremy Pry
+ * @license GPL2
+ */
+
+namespace Yikes\LevelPlayingField\Model;
+
+/**
+ * Interface ApplicantManagerMeta
+ *
+ * @since   %VERSION%
+ * @package Yikes\LevelPlayingField
+ */
+interface ApplicantManagerMeta {
+
+	const META_PREFIX = 'applicant_cpt_meta_';
+	const FORM_FIELD_PREFIX = 'applicant_cpt_';
+}
