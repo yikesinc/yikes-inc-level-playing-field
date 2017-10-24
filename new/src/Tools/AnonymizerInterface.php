677d4e4b635f0bf034c1c59e10f7ab8ea8f49541
Jeremy Pry <jeremy.pry@gmail.com>
2017-10-24 09:42:49 -0400

    Create Anonymizer interface

---
+++ new/src/Tools/AnonymizerInterface.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * YIKES Inc. Level Playing Field Plugin.
+ *
+ * @package Yikes\LevelPlayingField
+ * @author  Jeremy Pry
+ * @license GPL2
+ */
+
+namespace Yikes\LevelPlayingField\Tools;
+
+/**
+ * Interface AnonymizerInterface
+ *
+ * @since   %VERSION%
+ * @package Yikes\LevelPlayingField
+ */
+interface AnonymizerInterface {
+
+	/**
+	 * Get anonmyized data.
+	 *
+	 * @since %VERSION%
+	 *
+	 * @param mixed $data The data to anonymize.
+	 *
+	 * @return mixed
+	 */
+	public function anonymize( $data );
+
+	/**
+	 * Get original form of data.
+	 *
+	 * @since %VERSION%
+	 *
+	 * @param mixed $data The data to reveal.
+	 *
+	 * @return mixed
+	 */
+	public function reveal( $data );
+}
