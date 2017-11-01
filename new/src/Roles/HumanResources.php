c76a6fe6db68bc99415fce6b8bd8d08dbea21001
Jeremy Pry <jeremy.pry@gmail.com>
2017-11-01 11:32:15 -0400

    Create HR role

---
+++ new/src/Roles/HumanResources.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * YIKES Inc. Level Playing Field Plugin.
+ *
+ * @package Yikes\LevelPlayingField
+ * @author  Jeremy Pry
+ * @license GPL2
+ */
+
+namespace Yikes\LevelPlayingField\Roles;
+
+/**
+ * Class HumanResources
+ *
+ * @since   %VERSION%
+ * @package Yikes\LevelPlayingField
+ */
+class HumanResources extends BaseRole {
+
+	const SLUG = 'human-resources';
+
+	/**
+	 * Get the localized title for the role.
+	 *
+	 * @since %VERSION%
+	 * @return string
+	 */
+	protected function get_title() {
+		return esc_html__( 'Human Resources' );
+	}
+
+	/**
+	 * Get the capability array for the role.
+	 *
+	 * @since %VERSION%
+	 * @return array
+	 */
+	protected function get_caps() {
+		return [
+			Capabilities::VIEW_ANONYMIZED_APPLICATIONS => true,
+			Capabilities::MESSAGE_APPLICANTS           => true,
+			Capabilities::UNANONYMIZE                  => true,
+		];
+	}
+}
