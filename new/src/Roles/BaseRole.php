fc01b29b4b78bbe4b22d341b30658e4dac3bab48
Jeremy Pry <jeremy.pry@gmail.com>
2017-11-01 11:26:28 -0400

    Create separate ExistingRole abstraction to customize existing roles

--- new/src/Roles/BaseRole.php
+++ new/src/Roles/BaseRole.php
@@ -38,25 +38,6 @@ abstract class BaseRole implements Service {
 	 */
 	public function register_role() {
 		add_role( $this->get_slug(), $this->get_title(), $this->get_caps() );
-		foreach ( $this->get_additional_role_caps() as $role => $caps ) {
-			$_role = get_role( $role );
-
-			// If the role wasn't found, continue to the next role.
-			if ( null === $_role ) {
-				continue;
-			}
-
-			foreach ( $caps as $cap => $grant ) {
-				$grant = (bool) $grant;
-
-				// Prevent adding the capability if it's already present.
-				if ( array_key_exists( $cap, $_role->capabilities ) && $grant === $_role->capabilities[ $cap ] ) {
-					continue;
-				}
-
-				$_role->add_cap( $cap, $grant );
-			}
-		}
 	}
 
 	/**
@@ -90,32 +71,4 @@ abstract class BaseRole implements Service {
 	 * @return array
 	 */
 	abstract protected function get_caps();
-
-	/**
-	 * Get the additional roles and caps to register.
-	 *
-	 * This should return an associative array. The keys should be the role slug,
-	 * and the value should be an array of capabilities. The capabilities should
-	 * also be an associative array. This allows for setting capabilities with
-	 * a truthy value, and denying capabilities with a falsey value.
-	 *
-	 * Example:
-	 *
-	 * return array(
-	 *     'editor' => array(
-	 *         'custom_cap'     => true,
-	 *         'restricted_cap' => false,
-	 *     ),
-	 *     'administrator' => array(
-	 *         'custom_cap'     => true,
-	 *         'restricted_cap' => true,
-	 *     ),
-	 * );
-	 *
-	 * If no additional registrations are necessary, just return an empty array.
-	 *
-	 * @since %VERSION%
-	 * @return array
-	 */
-	abstract protected function get_additional_role_caps();
 }
