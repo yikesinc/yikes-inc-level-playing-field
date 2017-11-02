6727b5fea9be70bf824eba70586f311542e7abea
Jeremy Pry <jeremy.pry@gmail.com>
2017-11-02 08:14:16 -0400

    Simplify ExistingRole class to better follow parent

--- new/src/Roles/ExistingRole.php
+++ new/src/Roles/ExistingRole.php
@@ -23,52 +23,37 @@ abstract class ExistingRole extends BaseRole {
 	 * @since %VERSION%
 	 */
 	public function register_role() {
-		foreach ( $this->get_role_caps() as $role => $caps ) {
-			$_role = get_role( $role );
+		$role = get_role( $this->get_slug() );
 
-			// If the role wasn't found, continue to the next role.
-			if ( null === $_role ) {
+		// Since other code could interfere with roles, trigger an error instead of throwing an Exception.
+		if ( null === $role ) {
+			$message = sprintf( 'The %s role was not found.', $role );
+			trigger_error( $message, E_USER_NOTICE );
+
+			return;
+		}
+
+		foreach ( $this->get_caps() as $cap => $grant ) {
+			$grant = (bool) $grant;
+
+			// Prevent adding the capability if it's already present.
+			if ( array_key_exists( $cap, $role->capabilities ) && $grant === $role->capabilities[ $cap ] ) {
 				continue;
 			}
 
-			foreach ( $caps as $cap => $grant ) {
-				$grant = (bool) $grant;
-
-				// Prevent adding the capability if it's already present.
-				if ( array_key_exists( $cap, $_role->capabilities ) && $grant === $_role->capabilities[ $cap ] ) {
-					continue;
-				}
-
-				$_role->add_cap( $cap, $grant );
-			}
+			$role->add_cap( $cap, $grant );
 		}
 	}
 
 	/**
-	 * Get the additional roles and caps to register.
+	 * Get the localized title for the role.
 	 *
-	 * This should return an associative array. The keys should be the role slug,
-	 * and the value should be an array of capabilities. The capabilities should
-	 * also be an associative array. This allows for setting capabilities with
-	 * a truthy value, and denying capabilities with a falsey value.
-	 *
-	 * Example:
-	 *
-	 * return array(
-	 *     'editor' => array(
-	 *         'custom_cap'     => true,
-	 *         'restricted_cap' => false,
-	 *     ),
-	 *     'administrator' => array(
-	 *         'custom_cap'     => true,
-	 *         'restricted_cap' => true,
-	 *     ),
-	 * );
-	 *
-	 * If no additional registrations are necessary, just return an empty array.
+	 * This isn't used for existing roles.
 	 *
 	 * @since %VERSION%
-	 * @return array
+	 * @return string
 	 */
-	abstract protected function get_role_caps();
+	protected function get_title() {
+		return '';
+	}
 }
