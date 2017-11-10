f432a23df880501366d1b2a6ac1360e1a0b3bb4e
Jeremy Pry <jeremy.pry@gmail.com>
2017-11-10 13:27:55 -0500

    Add checking for the proper PHP version

--- yikes-level-playing-field.php
+++ yikes-level-playing-field.php
@@ -6,7 +6,7 @@
  * @license GPL2
  * @wordpress-plugin
  *
- * Plugin Name:  Level Playing Field 2
+ * Plugin Name:  Level Playing Field
  * Plugin URI:   http://www.yikesinc.com
  * Description:  A WordPress plugin to anonymize job applications to fight bias in hiring and employment.
  * Author:       YIKES, Inc.
@@ -22,6 +22,33 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die();
 }
 
+/*
+ * Require PHP 5.6. Checking here allows the plugin to fail gracefully.
+ *
+ * Note that this file needs to be compatible with PHP 5.2 at a minimum.
+ */
+if ( version_compare( '5.6.0', PHP_VERSION, '>' ) ) {
+	add_action( 'admin_notices', 'lpf_admin_notices' );
+
+	/**
+	 * Display admin notice for incompatible PHP version.
+	 *
+	 * @since %VERSION%
+	 */
+	function lpf_admin_notices() {
+		printf(
+			'<div class="error"><p>%s</p></div>',
+			sprintf(
+				/* translators: %1$s is the required PHP version, %2$s is the current version */
+				esc_html__( 'Yikes, Inc. Level Playing Fields requires PHP version %1$s or above. You site is using PHP version %2$s.' ),
+				'5.6.0',
+				esc_html( PHP_VERSION )
+			)
+		);
+	}
+
+	return;
+}
 
 // Bootstrap the plugin.
 require_once( dirname( __FILE__ ) . '/src/bootstrap.php' );
