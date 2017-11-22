0d1c2633867b3cf280b1ca97812a70dc3eda0afe
Jeremy Pry <jeremy.pry@gmail.com>
2017-11-22 07:38:39 -0500

    Update block class

--- src/Components/Block.php
+++ src/Components/Block.php
@@ -9,17 +9,41 @@
 
 namespace Yikes\LevelPlayingField\Model\Components;
 
+use Yikes\LevelPlayingField\Model\Components\Fields\Field;
+
+/**
+ * Class Block
+ *
+ * @since   %VERSION%
+ * @package Yikes\LevelPlayingField
+ */
 class Block {
 
+	use Disabled;
 	use Repeatable;
 
+	/**
+	 * Fields that are part of this block.
+	 *
+	 * @since %VERSION%
+	 * @var array
+	 */
 	protected $fields = array();
 
+	/**
+	 * Add a field to the block.
+	 *
+	 * @since %VERSION%
+	 *
+	 * @param Field $field The field object to add.
+	 */
 	public function add_field( Field $field ) {
 		$this->fields[ $field->get_key() ] = $field;
 	}
 
 	/**
+	 * Get the fields for this block.
+	 *
 	 * @return array
 	 */
 	public function get_fields() {
