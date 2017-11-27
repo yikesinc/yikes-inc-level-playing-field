a3aca0befd794a4e6b414ebdad11cf9bb0331479
Jeremy Pry <jeremy.pry@gmail.com>
2017-11-27 07:57:09 -0500

    Initial text field class

---
+++ src/Components/Fields/Text.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * YIKES Inc. Level Playing Field Plugin.
+ *
+ * @package Yikes\LevelPlayingField
+ * @author  Jeremy Pry
+ * @license GPL2
+ */
+
+namespace Yikes\LevelPlayingField\Model\Components\Fields;
+
+/**
+ * Class Text
+ *
+ * @since   %VERSION%
+ * @package Yikes\LevelPlayingField
+ */
+class Text extends BaseField {
+
+	const TYPE = 'text';
+
+
+}
