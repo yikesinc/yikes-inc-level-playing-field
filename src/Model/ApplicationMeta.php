40a4a5ea519ce2023a9d500dd12237d56887fa04
Jeremy Pry <jeremy.pry@gmail.com>
2018-03-20 07:36:34 -0400

    Add basic info fields

--- src/Model/ApplicationMeta.php
+++ src/Model/ApplicationMeta.php
@@ -19,4 +19,11 @@ interface ApplicationMeta {
 
 	const META_PREFIX       = 'application_cpt_meta_';
 	const FORM_FIELD_PREFIX = 'application_cpt_';
+
+	// Basic Info fields.
+	const NAME         = self::META_PREFIX . 'name';
+	const EMAIL        = self::META_PREFIX . 'email';
+	const PHONE        = self::META_PREFIX . 'phone';
+	const ADDRESS      = self::META_PREFIX . 'address';
+	const COVER_LETTER = self::META_PREFIX . 'cover_letter';
 }
