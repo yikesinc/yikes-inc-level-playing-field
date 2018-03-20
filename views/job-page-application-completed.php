d48d19459a6c64f54b67867600cf1337759349d1
Jeremy Pry <jeremy.pry@gmail.com>
2018-03-20 09:19:48 -0400

    Create initial file for displaying completed application page

---
+++ views/job-page-application-completed.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * YIKES Inc. Level Playing Field Plugin.
+ *
+ * @package Yikes\LevelPlayingField
+ * @author  Jeremy Pry
+ * @license GPL2
+ */
+
+namespace Yikes\LevelPlayingField;
+
+use Yikes\LevelPlayingField\Model\Application;
+
+?>
+
+
+
+
