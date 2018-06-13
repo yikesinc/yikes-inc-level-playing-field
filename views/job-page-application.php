de432adba30b23feb22fd2277068adff91ae0b64
Jeremy Pry <jeremy.pry@gmail.com>
2018-06-13 15:56:17 -0400

    Create and use a Factory for building array of field objects

--- views/job-page-application.php
+++ views/job-page-application.php
@@ -9,9 +9,8 @@
 
 namespace Yikes\LevelPlayingField;
 
+use Yikes\LevelPlayingField\Form\Factory;
 use Yikes\LevelPlayingField\Model\Application;
-use Yikes\LevelPlayingField\Model\ApplicantMeta as Meta;
-use Yikes\LevelPlayingField\Model\ApplicationMeta as AMeta;
 
 /** @var Application $application */
 $application = $this->application;
@@ -23,24 +22,16 @@ $field_classes = [
 	sprintf( 'lpf-application-%s', $application->get_id() ),
 ];
 
+$fields = Factory::create( $active_fields, $field_classes );
+
 ?>
 <form method="POST" id="<?php echo esc_attr( $application->get_id() ); ?>">
 	<?php wp_nonce_field( 'lpf_application_submit', 'lpf_nonce' ); ?>
 	<input type="hidden" name="application_id" value="<?php echo esc_attr( $application->get_id() ); ?>" />
 
 	<?php
-	foreach ( $active_fields as $field ) {
-		$type        = isset( Meta::FIELD_MAP[ $field ] ) ? Meta::FIELD_MAP[ $field ] : 'text';
-		$field_label = ucwords( str_replace( [ '-', '_' ], ' ', str_replace( AMeta::META_PREFIX, '', $field ) ) );
-		?>
-
-		<label><?php echo esc_html( $field_label ); ?>
-		<input type="<?php echo esc_attr( $type ); ?>"
-			   class="<?php echo esc_attr( join( ' ', array_merge( $field_classes, [ "lpf-field-{$type}" ] ) ) ); ?>"
-			   name="<?php echo esc_attr( $field ); ?>"
-			   id="<?php esc_attr( $field ); ?>" />
-		</label>
-		<?php
+	foreach ( $fields as $field ) {
+		$field->render();
 	}
 	?>
 	<button type="submit" name="lpf_submit"><?php esc_html_e( 'Submit' ); ?></button>
