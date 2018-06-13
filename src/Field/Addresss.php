be4fcc20a568ab1150cb618cbc63624c5645fd7f
Jeremy Pry <jeremy.pry@gmail.com>
2018-06-13 15:08:07 -0400

    Update address field

--- src/Field/Addresss.php
+++ src/Field/Addresss.php
@@ -9,6 +9,32 @@
 
 namespace Yikes\LevelPlayingField\Field;
 
-class Addresss {
+/**
+ * Class Addresss
+ *
+ * @since   %VERSION%
+ * @package Yikes\LevelPlayingField
+ */
+class Addresss extends BaseField {
 
+	/**
+	 * Render the field.
+	 *
+	 * @since %VERSION%
+	 */
+	public function render() {
+		$classes    = array_merge( $this->classes, 'lpf-field-address' );
+		$sub_fields = [
+			new Text( "{$this->id}[line_1]", '', $classes ),
+			new Text( "{$this->id}[line_2]", '', $classes, false ),
+			new Text( "{$this->id}[city]", '', $classes ),
+			new Text( "{$this->id}[state]", '', $classes ),
+			new Number( "{$this->id}[zip]", '', $classes ),
+		];
+
+		/** @var Field $sub_field */
+		foreach ( $sub_fields as $sub_field ) {
+			$sub_field->render();
+		}
+	}
 }
