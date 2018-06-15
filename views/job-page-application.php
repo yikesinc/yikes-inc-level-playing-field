48fdf086aeb048fc67a81a4f6a474e541939e283
Jeremy Pry <jeremy.pry@gmail.com>
2018-06-15 17:36:03 -0400

    Update how classes are applied to elements

--- views/job-page-application.php
+++ views/job-page-application.php
@@ -14,21 +14,27 @@ use Yikes\LevelPlayingField\Form\Factory;
 use Yikes\LevelPlayingField\Model\Application;
 
 /** @var Application $application */
-$application = $this->application;
-
+$application   = $this->application;
 $active_fields = $application->get_active_fields();
-$field_classes = [
-	'lpf-form-field',
+
+// Set up HTML classes.
+$base_classes  = [
 	'lpf-application',
 	sprintf( 'lpf-application-%s', $application->get_id() ),
 ];
+$field_classes = array_merge( [ 'lpf-form-field' ], $base_classes );
+$form_classes  = array_merge( [ 'lpf-form' ], $base_classes );
 
+// Set up Form Fields.
 // todo: change job_id to actual job, not application, ID.
 $fields   = Factory::create( $active_fields, $field_classes );
 $fields[] = new Hidden( 'job_id', $application->get_id() );
 
 ?>
-<form method="POST" id="<?php echo esc_attr( $application->get_id() ); ?>">
+<form method="POST"
+	  id="<?php echo esc_attr( $application->get_id() ); ?>"
+	  class="<?php echo esc_attr( join( ' ', $form_classes ) ); ?>"
+>
 	<?php
 	wp_nonce_field( 'lpf_application_submit', 'lpf_nonce' );
 	foreach ( $fields as $field ) {
