e8b58c1397a88c636b8514b9f2c698ecb41d830d
Jeremy Pry <jeremy.pry@gmail.com>
2018-06-22 08:43:24 -0400

    Make some updates to the Job template

--- views/job-page-job.php
+++ views/job-page-job.php
@@ -12,12 +12,14 @@ namespace Yikes\LevelPlayingField;
 use Yikes\LevelPlayingField\Model\Job;
 
 /** @var Job $job */
-$job = $this->job;
+$job        = $this->job;
+$show_title = (bool) apply_filters( 'lpf_single_job_template_show_title', true, $job );
+$use_comma  = (bool) apply_filters( 'lpf_single_job_template_address_use_comma', true, $job );
 ?>
 
 <div class="job-page-job">
 
-	<?php if ( apply_filters( 'lpf_single_job_template_show_title', true, $job ) === true ): ?>
+	<?php if ( $show_title ) : ?>
 		<h4 class="job-page-job-title"><?php echo esc_html( $job->get_title() ); ?></h4>
 	<?php endif; ?>
 
@@ -36,34 +38,35 @@ $job = $this->job;
 		<?php endif; ?>
 
 		<div class="job-page-job-address">
-
-			<?php if ( $job->is_remote() ) {
-
-				esc_html_e( 'Address: remote', 'yikes-level-playing-field' );
-
-			} else if ( ! empty( $address = $job->get_address() ) ) { ?>
-
+			<?php
+			if ( $job->is_remote() ) {
+				esc_html_e( 'Location: remote', 'yikes-level-playing-field' );
+			} elseif ( ! empty( $job->get_address() ) ) {
+				$address   = $job->get_address();
+				$use_comma = $use_comma && ! empty( $address['city'] ) && ( ! empty( $address['state'] ) || ! empty( $address['province'] ) );
+				?>
 				<address id="lpf-address">
-
 					<div id="lpf-address1"><?php echo esc_html( $address['address-1'] ); ?></div>
-
 					<div id="lpf-address2"><?php echo esc_html( $address['address-2'] ); ?></div>
-
-					<span id="lpf-city"><?php echo esc_html( $address['city'] ); ?></span><?php if ( ! empty( $address['state'] ) ){ ?><span id="lpf-city-state-comma">,</span><?php } ?>
-
-					<span id="lpf-province"><?php echo esc_html( $address['province'] ); ?></span><?php if ( ! empty( $address['province'] ) ) { ?><span id="lpf-city-province-comma">,</span><?php } ?>
-
-					<span id="lpf-city"><?php echo esc_html( $address['state'] ); ?></span>
+					<span id="lpf-city"><?php echo esc_html( $address['city'] ); ?></span>
+					<?php if ( ! empty( $address['province'] ) ) : ?>
+						<span id="lpf-province"><?php echo esc_html( $address['province'] ); ?></span>
+						<?php if ( $use_comma ) : ?>
+							<span id="lpf-city-province-comma">,</span>
+						<?php endif; ?>
+					<?php elseif ( ! empty( $address['state'] ) ) : ?>
+						<span id="lpf-city"><?php echo esc_html( $address['state'] ); ?></span>
+						<?php if ( $use_comma ) : ?>
+							<span id="lpf-city-state-comma">,</span>
+						<?php endif; ?>
+					<?php endif; ?>
 
 					<div id="lpf-country"><?php echo esc_html( $address['country'] ); ?></div>
 					<div id="lpf-zip"><?php echo esc_html( $address['zip'] ); ?></div>
-
 				</address>
-
-			<?php }; ?>
-
+			<?php } ?>
 		</div>
 
 	</div>
-	<!-- Output application form -->
+	<!-- todo: Output application form -->
 </div>
