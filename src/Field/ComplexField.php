78a3e01c98e600bbd4d09492f92ad30076c44101
Jeremy Pry <jeremy.pry@gmail.com>
2018-07-04 15:39:21 -0400

    Break complex fields into smaller methods

--- src/Field/ComplexField.php
+++ src/Field/ComplexField.php
@@ -20,18 +20,46 @@ use Yikes\LevelPlayingField\Exception\InvalidField;
 abstract class ComplexField extends BaseField {
 
 	/**
-	 * Render the field.
+	 * Whether the field is repeatable.
+	 *
+	 * @since %VERSION%
+	 * @var bool
+	 */
+	protected $repeatable = false;
+
+	/**
+	 * Array of sub-fields.
+	 *
+	 * @since %VERSION%
+	 * @var array
+	 */
+	protected $sub_fields = [];
+
+	/**
+	 * BaseField constructor.
+	 *
+	 * @param string $id       The field ID.
+	 * @param string $label    The field label.
+	 * @param array  $classes  Array of field classes.
+	 * @param bool   $required Whether the field is required.
+	 *
+	 * @throws InvalidField When the provided ID is invalid.
+	 */
+	public function __construct( $id, $label, array $classes, $required = true ) {
+		parent::__construct( $id, $label, $classes, $required );
+		$this->generate_sub_fields();
+	}
+
+	/**
+	 * Generate the sub-field objects for this field.
 	 *
 	 * @since %VERSION%
 	 *
 	 * @throws InvalidField When an invalid field class is provided through the filter.
 	 */
-	public function render() {
+	protected function generate_sub_fields() {
 		$classes        = array_merge( $this->classes, $this->get_classes() );
 		$default_fields = $this->get_default_fields();
-
-		// Generate the sub-fields for the address.
-		$sub_fields = [];
 		foreach ( $default_fields as $field => $settings ) {
 			$settings = wp_parse_args( $settings, [
 				'label'    => ucwords( str_replace( [ '_', '-' ], ' ', $field ) ),
@@ -39,28 +67,42 @@ abstract class ComplexField extends BaseField {
 				'required' => true,
 			] );
 
-			$sub_fields[] = new $settings['class'](
-				"{$this->id}[{$field}]",
+			// Set up the field ID, depending on whether the field is repeatable.
+			$id = $this->id . ( $this->repeatable ? '[]' : '' ) . "[{$field}]";
+
+			// Instantiate the sub field.
+			$this->sub_fields[] = new $settings['class'](
+				$id,
 				$settings['label'],
 				$classes,
 				(bool) $settings['required']
 			);
 
 			// Ensure the class extends the Field interface.
-			if ( ! ( end( $sub_fields ) instanceof Field ) ) {
+			if ( ! ( end( $this->sub_fields ) instanceof Field ) ) {
 				throw InvalidField::from_field( $settings['class'] );
 			}
 		}
+	}
 
-		// Render the grouping label.
+	/**
+	 * Render the field.
+	 *
+	 * @since %VERSION%
+	 */
+	public function render() {
 		$this->render_grouping_label();
+		$this->render_sub_fields();
+	}
 
-		/**
-		 * Render the sub-fields.
-		 *
-		 * @var Field $sub_field
-		 */
-		foreach ( $sub_fields as $sub_field ) {
+	/**
+	 * Render the sub-fields.
+	 *
+	 * @since %VERSION%
+	 */
+	protected function render_sub_fields() {
+		/** @var Field $sub_field */
+		foreach ( $this->sub_fields as $sub_field ) {
 			$sub_field->render();
 		}
 	}
