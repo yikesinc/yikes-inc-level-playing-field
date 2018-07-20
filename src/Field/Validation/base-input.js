e226cf05f174847ba2123916df1cdceebad131ad
Ebonie <ebonie@metalandcoffee.com>
2018-07-20 14:57:32 -0400

    Base Validation Framework

---
+++ src/Field/Validation/base-input.js
@@ -0,0 +1,8 @@
+// Base Input Field Validation. Trim whitespace and check for empty required fields.
+
+jQuery(document).ready(function($) {
+    $(window).load(function(){
+        const allFields = $('lpf-form-field');
+        console.log( allFields );
+    });
+});
