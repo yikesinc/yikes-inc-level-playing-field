412be6c1a1c3257d212329c990d30cf27a6d4e80
Ebonie Butler <ebonie@metalandcoffee.com>
2018-07-23 10:43:58 -0400

    check if inputs are empty basics

--- src/Field/Validation/base-input.js
+++ src/Field/Validation/base-input.js
@@ -2,7 +2,22 @@
 
 jQuery(document).ready(function($) {
     $(window).load(function(){
-        const allFields = $('lpf-form-field');
-        console.log( allFields );
+        // Variables
+        const allFields = $('.lpf-form-field');
+        const submitBtn = $('.lpf-submit');
+        const submitValidation = (event) => {
+            allFields.each( function() {
+                // Trim whitespace
+                trimmedValue = $.trim(this.value);
+                // If empty...
+                if (!trimmedValue) {
+                    console.log('empty');
+                }
+            });
+            event.preventDefault();
+        }
+
+        // When 'Submit' button is clicked...
+        submitBtn.on('click', submitValidation);
     });
 });
