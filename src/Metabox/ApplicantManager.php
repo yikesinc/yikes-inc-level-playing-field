b386d88dc39525d142578c882b3b29065b060967
Jeremy Pry <jeremy.pry@gmail.com>
2018-09-14 11:12:38 -0400

    Update HTML markup and add some placeholder data
    (cherry picked from commit 49d9b0c32d70d1d8fa1281f3938fdcf83f8fa9af)

--- src/Metabox/ApplicantManager.php
+++ src/Metabox/ApplicantManager.php
@@ -14,10 +14,11 @@ use Yikes\LevelPlayingField\Assets\Asset;
 use Yikes\LevelPlayingField\Assets\AssetsAware;
 use Yikes\LevelPlayingField\Assets\AssetsAwareness;
 use Yikes\LevelPlayingField\Assets\ScriptAsset;
+use Yikes\LevelPlayingField\Assets\StyleAsset;
 use Yikes\LevelPlayingField\CustomPostType\ApplicantManager as ApplicantCPT;
+use Yikes\LevelPlayingField\Model\Applicant;
 use Yikes\LevelPlayingField\Service;
 use Yikes\LevelPlayingField\Taxonomy\ApplicantStatus;
-use Yikes\LevelPlayingField\Assets\StyleAsset;
 
 /**
  * Class ApplicantManager
@@ -72,96 +73,125 @@ final class ApplicantManager implements AssetsAware, Service {
 	 * @since %VERSION%
 	 */
 	private function do_applicant_content() {
-		$html = '<article id="single-applicant-view">';
-		//$html .= 'Applicant content';
-		$html .= '<section id="header">';
-		$html .= '<img class="avatar" src="https://via.placeholder.com/350x350" alt="Placeholder" />';
-		$html .= '<h5>Nickname 123</h5>';
-		$html .= '<h5><span class="label">Job:</span>';
-		$html .= 'Job Title</h5>';
-		$html .= '</section>';
-		$html .= '<section id="basic-info">';
-		$html .= '<h2>Basic Info</h2>';
-		$html .= '<p class="location"><span class="label">Location:</span>';
-		$html .= 'City, ';
-		$html .= 'State</p>';
-		$html .= '</section>';
-		$html .= '<section id="education">';
-		$html .= '<h2>Education</h2>';
-		$html .= '<h5>Schooling</h5>';
-		$html .= '<ol>';
-		$html .= '<li>Graduated with a [ degree ] from [ institution type ] with a major in [ major ]</li>';
-		$html .= '<li>Graduated with a [ degree ] from [ institution type ] with a major in [ major ]</li>';
-		$html .= '</ol>';
-		$html .= '<h5>Certifications</h5>';
-		$html .= '<ol>';
-		$html .= '<li>Certified in [ certification ] from [ institution type ]. Status: [ status ]</li>';
-		$html .= '<li>Certified in [ certification ] from [ institution type ]. Status: [ status ]</li>';
-		$html .= '<li>Certified in [ certification ] from [ institution type ]. Status: [ status ]</li>';
-		$html .= '</ol>';
-		$html .= '</section>';
-		$html .= '<section id="skills">';
-		$html .= '<h2>Skills</h2>';
-		$html .= '<table>';
-		$html .= '<tr>';
-		$html .= '<th>Skill</th>';
-		$html .= '<th>Proficiency</th>';
-		$html .= '</tr>';
-		$html .= '<tr>';
-		$html .= '<td>[ skill ]</td>';
-		$html .= '<td>[ proficiency ]</td>';
-		$html .= '</tr>';
-		$html .= '<tr>';
-		$html .= '<td>[ skill ]</td>';
-		$html .= '<td>[ proficiency ]</td>';
-		$html .= '</tr>';
-		$html .= '<tr>';
-		$html .= '<td>[ skill ]</td>';
-		$html .= '<td>[ proficiency ]</td>';
-		$html .= '</tr>';
-		$html .= '</table>';
-		$html .= '</section>';
-		$html .= '<section id="languages">';
-		$html .= '<h2>Languages</h2>';
-		$html .= '<h5>Multingual</h5>';
-		$html .= '<ol>';
-		$html .= '<li>[ fluency ] x languages</li>';
-		$html .= '<li>Fluent in 2 languages</li>';
-		$html .= '<li>Limited proficiency in 1 language</li>';
-		$html .= '</ol>';
-		$html .= '</section>';
-		$html .= '<section id="experience">';
-		$html .= '<h2>Experience</h2>';
-		$html .= '<ol>';
-		$html .= '<li>[ position ] in [ industry ] for x years</li>';
-		$html .= '<li>[ position ] in [ industry ] for x years</li>';
-		$html .= '<li>[ position ] in [ industry ] for x years</li>';
-		$html .= '</ol>';
-		$html .= '</section>';
-		$html .= '<section id="volunteer-work">';
-		$html .= '<h2>Volunteer Work</h2>';
-		$html .= '<ol>';
-		$html .= '<li>[ position ] in [ organization type ] for x years</li>';
-		$html .= '<li>[ position ] in [ organization type ] for x years</li>';
-		$html .= '</ol>';
-		$html .= '</section>';
-		$html .= '<section id="misc">';
-		$html .= '<h2>Miscellaneous</h2>';
-		$html .= '<p><span class="label">Question:</span>';
-		$html .= 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?</p>';
-		$html .= '<p><span class="label">Answer:</span>';
-		$html .= 'Vivamus nec ex volutpat, porta libero ut, malesuada lectus.</p>';
-		$html .= '<p><span class="label">Question:</span>';
-		$html .= 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?</p>';
-		$html .= '<p><span class="label">Answer:</span>';
-		$html .= 'Vivamus nec ex volutpat, porta libero ut, malesuada lectus.</p>';
-		$html .= '</section>';
-		$html .= '</article>';
-		echo $html;
+		$applicant = new Applicant( get_post() );
+
+		// Trigger loading of applicant data.
+		$applicant->schooling;
 
 		// @todo: this isn't the proper way to display the taxonomy box; change it.
 		$status = new ApplicantStatus();
 		$status->meta_box_cb( get_post() );
+
+		// Placeholder data.
+		$applicant->schooling = [
+			[
+				'degree' => 'Diploma',
+				'type'   => 'High School',
+				'major'  => 'n/a',
+			],
+			[
+				'degree' => 'B.S.',
+				'type'   => 'College',
+				'major'  => 'Accounting',
+			],
+		];
+		?>
+
+		<article id="single-applicant-view">
+			<section id="header">
+				<?php echo $applicant->get_avatar_img( 350 ); // XSS ok. ?>
+				<h5>Nickname 123</h5>
+				<h5><span class="label">Job:</span>
+					Job Title</h5>
+			</section>
+			<section id="basic-info">
+				<h2>Basic Info</h2>
+				<p class="location"><span class="label">Location:</span>
+					City,
+					State</p>
+			</section>
+			<section id="education">
+				<h2>Education</h2>
+				<h5>Schooling</h5>
+				<ol>
+					<?php
+					foreach ( $applicant->get_schooling() as $schooling ) {
+						printf(
+							'<li>Graduated with a [%s] from [%s] with a major in [%s]',
+							esc_html( $schooling['degree'] ),
+							esc_html( $schooling['type'] ),
+							esc_html( $schooling['major'] )
+						);
+					}
+					?>
+				</ol>
+				<h5>Certifications</h5>
+				<ol>
+					<li>Certified in [ certification ] from [ institution type ]. Status: [ status ]</li>
+					<li>Certified in [ certification ] from [ institution type ]. Status: [ status ]</li>
+					<li>Certified in [ certification ] from [ institution type ]. Status: [ status ]</li>
+				</ol>
+			</section>
+			<section id="skills">
+				<h2>Skills</h2>
+				<table>
+					<tr>
+						<th>Skill</th>
+						<th>Proficiency</th>
+					</tr>
+					<tr>
+						<td>[ skill ]</td>
+						<td>[ proficiency ]</td>
+					</tr>
+					<tr>
+						<td>[ skill ]</td>
+						<td>[ proficiency ]</td>
+					</tr>
+					<tr>
+						<td>[ skill ]</td>
+						<td>[ proficiency ]</td>
+					</tr>
+				</table>
+			</section>
+			<section id="languages">
+				<h2>Languages</h2>
+				<h5>Multingual</h5>
+				<ol>
+					<li>[ fluency ] x languages</li>
+					<li>Fluent in 2 languages</li>
+					<li>Limited proficiency in 1 language</li>
+				</ol>
+			</section>
+			<section id="experience">
+				<h2>Experience</h2>
+				<ol>
+					<li>[ position ] in [ industry ] for x years</li>
+					<li>[ position ] in [ industry ] for x years</li>
+					<li>[ position ] in [ industry ] for x years</li>
+				</ol>
+			</section>
+			<section id="volunteer-work">
+				<h2>Volunteer Work</h2>
+				<ol>
+					<li>[ position ] in [ organization type ] for x years</li>
+					<li>[ position ] in [ organization type ] for x years</li>
+				</ol>
+			</section>
+			<section id="misc">
+				<h2>Miscellaneous</h2>
+				<p><span class="label">Question:</span>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit?</p>
+				<p><span class="label">Answer:</span>
+					Vivamus nec ex volutpat, porta libero ut, malesuada lectus.</p>
+				<p><span class="label">Question:</span>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit?</p>
+				<p><span class="label">Answer:</span>
+					Vivamus nec ex volutpat, porta libero ut, malesuada lectus.</p>
+			</section>
+		</article>
+		
+		
+		<?php
 	}
 
 	/**
