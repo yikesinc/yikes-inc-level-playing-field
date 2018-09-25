f35d52b2d38b8b4b3a2ec9eef12acd562ae84ddc
Jeremy Pry <jeremy.pry@gmail.com>
2018-09-25 09:04:42 -0400

    Adjust some of the native roles

--- src/Roles/Administrator.php
+++ src/Roles/Administrator.php
@@ -31,7 +31,7 @@ final class Administrator extends ExistingRole {
 			Capabilities::MESSAGE_APPLICANTS            => true,
 			Capabilities::UNANONYMIZE                   => false,
 
-			// CPT capabilities.
+			// Job capabilities.
 			Capabilities::EDIT_JOB                      => true,
 			Capabilities::EDIT_JOBS                     => true,
 			Capabilities::EDIT_OTHERS_JOBS              => true,
@@ -46,6 +46,8 @@ final class Administrator extends ExistingRole {
 			Capabilities::EDIT_PRIVATE_JOBS             => true,
 			Capabilities::EDIT_PUBLISHED_JOBS           => true,
 			Capabilities::CREATE_JOBS                   => true,
+
+			// Application capabilities.
 			Capabilities::EDIT_APPLICATION              => true,
 			Capabilities::EDIT_APPLICATIONS             => true,
 			Capabilities::EDIT_OTHERS_APPLICATIONS      => true,
@@ -60,6 +62,8 @@ final class Administrator extends ExistingRole {
 			Capabilities::EDIT_PRIVATE_APPLICATIONS     => true,
 			Capabilities::EDIT_PUBLISHED_APPLICATIONS   => true,
 			Capabilities::CREATE_APPLICATIONS           => true,
+
+			// Applicant capabilities.
 			Capabilities::EDIT_APPLICANT                => true,
 			Capabilities::EDIT_APPLICANTS               => true,
 			Capabilities::EDIT_OTHERS_APPLICANTS        => true,
@@ -73,7 +77,7 @@ final class Administrator extends ExistingRole {
 			Capabilities::DELETE_OTHERS_APPLICANTS      => true,
 			Capabilities::EDIT_PRIVATE_APPLICANTS       => true,
 			Capabilities::EDIT_PUBLISHED_APPLICANTS     => true,
-			Capabilities::CREATE_APPLICANTS             => true,
+			Capabilities::CREATE_APPLICANTS             => false,
 
 			// Admin Pages.
 			Capabilities::VIEW_ADMIN_PAGES              => true,
--- src/Roles/Editor.php
+++ src/Roles/Editor.php
@@ -31,7 +31,7 @@ final class Editor extends ExistingRole {
 			Capabilities::MESSAGE_APPLICANTS            => false,
 			Capabilities::UNANONYMIZE                   => false,
 
-			// CPT capabilities.
+			// Job capabilities.
 			Capabilities::EDIT_JOB                      => true,
 			Capabilities::EDIT_JOBS                     => true,
 			Capabilities::EDIT_OTHERS_JOBS              => true,
@@ -46,6 +46,8 @@ final class Editor extends ExistingRole {
 			Capabilities::EDIT_PRIVATE_JOBS             => true,
 			Capabilities::EDIT_PUBLISHED_JOBS           => true,
 			Capabilities::CREATE_JOBS                   => true,
+
+			// Application capabilities.
 			Capabilities::EDIT_APPLICATION              => true,
 			Capabilities::EDIT_APPLICATIONS             => true,
 			Capabilities::EDIT_OTHERS_APPLICATIONS      => true,
@@ -60,20 +62,22 @@ final class Editor extends ExistingRole {
 			Capabilities::EDIT_PRIVATE_APPLICATIONS     => true,
 			Capabilities::EDIT_PUBLISHED_APPLICATIONS   => true,
 			Capabilities::CREATE_APPLICATIONS           => true,
-			Capabilities::EDIT_APPLICANT                => true,
-			Capabilities::EDIT_APPLICANTS               => true,
-			Capabilities::EDIT_OTHERS_APPLICANTS        => true,
-			Capabilities::PUBLISH_APPLICANTS            => true,
-			Capabilities::READ_APPLICANT                => true,
-			Capabilities::READ_PRIVATE_APPLICANTS       => true,
-			Capabilities::DELETE_APPLICANT              => true,
-			Capabilities::DELETE_APPLICANTS             => true,
-			Capabilities::DELETE_PRIVATE_APPLICANTS     => true,
-			Capabilities::DELETE_PUBLISHED_APPLICANTS   => true,
-			Capabilities::DELETE_OTHERS_APPLICANTS      => true,
-			Capabilities::EDIT_PRIVATE_APPLICANTS       => true,
-			Capabilities::EDIT_PUBLISHED_APPLICANTS     => true,
-			Capabilities::CREATE_APPLICANTS             => true,
+
+			// Applicant capabilities.
+			Capabilities::EDIT_APPLICANT                => false,
+			Capabilities::EDIT_APPLICANTS               => false,
+			Capabilities::EDIT_OTHERS_APPLICANTS        => false,
+			Capabilities::PUBLISH_APPLICANTS            => false,
+			Capabilities::READ_APPLICANT                => false,
+			Capabilities::READ_PRIVATE_APPLICANTS       => false,
+			Capabilities::DELETE_APPLICANT              => false,
+			Capabilities::DELETE_APPLICANTS             => false,
+			Capabilities::DELETE_PRIVATE_APPLICANTS     => false,
+			Capabilities::DELETE_PUBLISHED_APPLICANTS   => false,
+			Capabilities::DELETE_OTHERS_APPLICANTS      => false,
+			Capabilities::EDIT_PRIVATE_APPLICANTS       => false,
+			Capabilities::EDIT_PUBLISHED_APPLICANTS     => false,
+			Capabilities::CREATE_APPLICANTS             => false,
 
 			// Admin Pages.
 			Capabilities::VIEW_ADMIN_PAGES              => true,
