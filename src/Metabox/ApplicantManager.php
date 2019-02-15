24e0771580e9bdf124357b8f8896ebb07ee4ae30
Tracy Levesque <tracy@yikesinc.com>
2019-02-15 16:56:21 -0500

    First round of layout and style edits.

--- src/Metabox/ApplicantManager.php
+++ src/Metabox/ApplicantManager.php
@@ -116,190 +116,252 @@ final class ApplicantManager implements AssetsAware, Service {
 		$applicant = ( new ApplicantRepository() )->find( get_the_ID() );
 		$job       = ( new JobRepository() )->find( $applicant->get_job_id() );
 		?>
-		<article id="single-applicant-view">
-			<section id="header">
-				<?php echo $applicant->get_avatar_img( 160 ); //phpcs:ignore WordPress.Security.EscapeOutput ?>
-				<h5>
-					<span class="label"><?php esc_html_e( 'Nickname:', 'yikes-level-playing-field' ); ?></span>
-					<span id="editable-nick-name"><?php echo esc_html( $applicant->get_nickname() ); ?></span>
-					<span id="edit-nickname-buttons">
-						<button type="button" class="edit-nickname button button-small hide-if-no-js" aria-label="Edit nickname"><?php esc_html_e( 'Edit', 'yikes-level-playing-field' ); ?></button>
-					</span>
-				</h5>
-				<h5>
-					<span class="label"><?php esc_html_e( 'Job:', 'yikes-level-playing-field' ); ?></span>
-					<?php echo esc_html( $job->get_title() ); ?>
-				</h5>
-			</section>
-			<?php do_action( "lpf_{$this->get_post_type()}_after_header", $applicant, $job ); ?>
-			<section id="basic-info">
-				<h2><?php esc_html_e( 'Basic Info', 'yikes-level-playing-field' ); ?></h2>
-				<p class="location"><span class="label"><?php esc_html_e( 'Location:', 'yikes-level-playing-field' ); ?></span>
-					<?php
-					foreach ( $applicant->get_address() as $field ) {
-						echo esc_html( $field ), '<br>';
-					}
-					?>
-				</p>
-				<p class="cover-letter">
-					<span class="label"><?php esc_html_e( 'Cover Letter:', 'yikes-level-playing-field' ); ?></span>
-					<a href="#"><?php esc_html_e( 'View Cover Letter', 'yikes-level-playing-field' ); ?></a>
-				</p>
-				<?php
-				// @todo: Should HTML be allowed in the cover letter?
-				?>
-				<div class="cover-letter-content">
-					<?php echo esc_html( $applicant->get_cover_letter() ); ?>
-				</div>
-			</section>
-			<?php do_action( "lpf_{$this->get_post_type()}_after_basic_info", $applicant, $job ); ?>
-			<section id="education">
-				<h2><?php esc_html_e( 'Education', 'yikes-level-playing-field' ); ?></h2>
-				<h5><?php esc_html_e( 'Schooling', 'yikes-level-playing-field' ); ?></h5>
-				<ol>
-					<?php
-					foreach ( $applicant->get_schooling() as $schooling ) {
-						printf(
-							'<li>Graduated with a %s from %s with a major in %s</li>',
-							esc_html( $schooling['degree'] ),
-							esc_html( $schooling['type'] ),
-							esc_html( $schooling['major'] )
-						);
-					}
-					?>
-				</ol>
-				<h5><?php esc_html_e( 'Certifications', 'yikes-level-playing-field' ); ?></h5>
-				<ol>
-					<?php
-					foreach ( $applicant->get_certifications() as $certification ) {
-						printf(
-							'<li>Certified in %s from %s. Status: %s</li>',
-							esc_html( $certification['certification_type'] ),
-							esc_html( $certification['type'] ),
-							esc_html( $certification['status'] )
-						);
-					}
-					?>
-				</ol>
-			</section>
-			<?php do_action( "lpf_{$this->get_post_type()}_after_education", $applicant, $job ); ?>
-			<section id="skills">
-				<h2><?php esc_html_e( 'Skills', 'yikes-level-playing-field' ); ?></h2>
-				<table>
-					<tr>
-						<th><?php esc_html_e( 'Skill', 'yikes-level-playing-field' ); ?></th>
-						<th><?php esc_html_e( 'Proficiency', 'yikes-level-playing-field' ); ?></th>
-					</tr>
-					<?php
-					foreach ( $applicant->get_skills() as $skill ) {
-						printf(
-							'<tr><td>%s</td><td>%s</td></tr>',
-							esc_html( $skill['skill'] ),
-							esc_html( $skill['proficiency'] )
-						);
-					}
-					?>
-				</table>
-			</section>
-			<?php do_action( "lpf_{$this->get_post_type()}_after_skills", $applicant, $job ); ?>
-			<section id="languages">
-				<?php
-				$languages       = $applicant->get_languages();
-				$is_multilingual = count( $languages ) > 1;
+		<div id="poststuff" class="single-applicant-view">
+			<div class="metabox-holder columns-2" id="post-body">
+				<div class="postbox-container" id="postbox-container-1">
+					<div class="meta-box-sortables ui-sortable" id="side-sortables">
+						<?php do_action( "lpf_{$this->get_post_type()}_after_header", $applicant, $job ); ?>
+						<div id="basic-info" class="postbox">
+							<h2 class="hndle ui-sortable-handle">
+								<span>
+									<?php esc_html_e( 'Basic Applicant Information', 'yikes-level-playing-field' ); ?>
+								</span>
+							</h2>
+							<div class="inside">
+								<p class="location">
+									<span class="label"><?php esc_html_e( 'Location:', 'yikes-level-playing-field' ); ?></span>
+									<?php
+									foreach ( $applicant->get_address() as $field ) {
+										echo esc_html( $field ), '<br>';
+									}
+									?>
+								</p>
+								<p class="cover-letter">
+									<span class="label"><?php esc_html_e( 'Cover Letter:', 'yikes-level-playing-field' ); ?></span>
+									<a href="#"><?php esc_html_e( 'View Cover Letter', 'yikes-level-playing-field' ); ?></a>
+								</p>
+								<?php
+								// @todo: Should HTML be allowed in the cover letter?
+								?>
+								<div class="cover-letter-content">
+									<?php echo esc_html( $applicant->get_cover_letter() ); ?>
+								</div>
+							</div><!-- /inside -->
+						</div><!-- /postbox -->
 
-				// Build up language proficiency data.
-				$proficiency_labels = [
-					'fluent'       => __( 'Fluent', 'yikes-level-playing-field' ),
-					'professional' => __( 'Working Professional proficiency', 'yikes-level-playing-field' ),
-					'limited'      => __( 'Limited proficiency', 'yikes-level-playing-field' ),
-					'elementary'   => __( 'Elementary proficiency', 'yikes-level-playing-field' ),
-				];
+						<div id="interview" class="postbox">
+							<h2 class="hndle ui-sortable-handle">
+								<span>
+									<?php esc_html_e( 'Interview Details', 'yikes-level-playing-field' ); ?>
+								</span>
+							</h2>
+							<div class="inside">
+								<?php do_action( "lpf_{$this->get_post_type()}_after_misc", $applicant, $job ); ?>
+								<?php if ( $applicant->get_interview_status() === 'scheduled' || $applicant->get_interview_status() === 'confirmed' ) { ?>
+									<?php // @todo: fetch interview data with $applicant->get_interview(). ?>
+									<?php $interview = maybe_unserialize( $applicant->__get( 'interview' ) ); ?>
+									<p><span class="label"><?php esc_html_e( 'Date:', 'yikes-level-playing-field' ); ?></span>
+									<?php echo esc_html( $interview['date'] ); ?></p>
+									<p><span class="label"><?php esc_html_e( 'Time:', 'yikes-level-playing-field' ); ?></span>
+										<?php echo esc_html( $interview['time'] ); ?></p>
+									<p><span class="label"><?php esc_html_e( 'Location:', 'yikes-level-playing-field' ); ?></span>
+										<?php echo esc_html( $interview['location'] ); ?></p>
+									<p><span class="label"><?php esc_html_e( 'Message:', 'yikes-level-playing-field' ); ?></span>
+										<?php echo esc_html( $interview['message'] ); ?></p>
+								<?php } else { ?>
+									<p><span class="label">An interview has not been scheduled yet.</span>
+								<?php } ?>
+								<?php do_action( "lpf_{$this->get_post_type()}_after_interview", $applicant, $job ); ?>
+							</div>
+						</div>
+					</div><!-- /meta-box-sortables -->
+				</div><!-- /postbox-container-1 -->
 
-				$proficiency_counts = [];
-				foreach ( $languages as $language ) {
-					if ( ! array_key_exists( $language['proficiency'], $proficiency_counts ) ) {
-						$proficiency_counts[ $language['proficiency'] ] = 1;
-						continue;
-					}
+				<div class="postbox-container" id="postbox-container-2">
+					<div class="meta-box-sortables ui-sortable" id="normal-sortables">
+						<!-- Avatar, nicknme and associated job -->
+						<div id="basic-info" class="postbox">
+							<div class="inside">
+								<section id="header">
+									<?php echo $applicant->get_avatar_img( 120 ); //phpcs:ignore WordPress.Security.EscapeOutput ?>
+									<h5>
+										<span class="label"><?php esc_html_e( 'Nickname:', 'yikes-level-playing-field' ); ?></span>
+										<span id="editable-nick-name"><?php echo esc_html( $applicant->get_nickname() ); ?></span>
+										<span id="edit-nickname-buttons">
+											<button type="button" class="edit-nickname button button-small hide-if-no-js" aria-label="Edit nickname"><?php esc_html_e( 'Edit Nickname', 'yikes-level-playing-field' ); ?></button>
+										</span>
+									</h5>
+									<h5>
+										<span class="label"><?php esc_html_e( 'Job:', 'yikes-level-playing-field' ); ?></span>
+										<?php echo esc_html( $job->get_title() ); ?>
+									</h5>
+								</section><!-- /header -->
+							</div><!-- /inside -->							
+							<br class="clear">
+							</br>
+						</div><!-- /postbox -->
 
-					$proficiency_counts[ $language['proficiency'] ]++;
-				}
+						<!-- Applicant Skills and Qualifications -->
+						<div class="postbox">
+							<h2 class="hndle ui-sortable-handle">
+								<span>
+									<?php esc_html_e( 'Skills and Qualifications', 'yikes-level-playing-field' ); ?>
+								</span>
+							</h2>
+							<div class="inside">
+								<div class="yks_mbox">
+									<?php do_action( "lpf_{$this->get_post_type()}_after_basic_info", $applicant, $job ); ?>
+									<section id="education">
+										<h4 class="yks_mbox_title"><?php esc_html_e( 'Education', 'yikes-level-playing-field' ); ?></h4>
+										<div class="applicant-skills-container">
+											<h5><?php esc_html_e( 'Schooling', 'yikes-level-playing-field' ); ?></h5>
+											<ol>
+												<?php
+												foreach ( $applicant->get_schooling() as $schooling ) {
+													printf(
+														'<li>Graduated with a %s from %s with a major in %s</li>',
+														esc_html( $schooling['degree'] ),
+														esc_html( $schooling['type'] ),
+														esc_html( $schooling['major'] )
+													);
+												}
+												?>
+											</ol>
 
-				// Set up a counter for when we need to output a comma.
-				$needs_comma = count( $proficiency_counts ) - 1;
+											<h5><?php esc_html_e( 'Certifications', 'yikes-level-playing-field' ); ?></h5>
+											<ol>
+												<?php
+												foreach ( $applicant->get_certifications() as $certification ) {
+													printf(
+														'<li>Certified in %s from %s. Status: %s</li>',
+														esc_html( $certification['certification_type'] ),
+														esc_html( $certification['type'] ),
+														esc_html( $certification['status'] )
+													);
+												}
+												?>
+											</ol>
+										</div>
+									</section>
+									<?php do_action( "lpf_{$this->get_post_type()}_after_education", $applicant, $job ); ?>
+									<section id="skills">
+										<h4 class="yks_mbox_title"><?php esc_html_e( 'Skills', 'yikes-level-playing-field' ); ?></h4>
+										<div class="applicant-skills-container">
+											<table class="wp-list-table widefat fixed striped users">
+												<thead>
+													<tr>
+														<th><?php esc_html_e( 'Skill', 'yikes-level-playing-field' ); ?></th>
+														<th><?php esc_html_e( 'Proficiency', 'yikes-level-playing-field' ); ?></th>
+													</tr>
+												</thead>
+												<tbody>
+													<?php
+													foreach ( $applicant->get_skills() as $skill ) {
+														printf(
+															'<tr><td>%s</td><td>%s</td></tr>',
+															esc_html( $skill['skill'] ),
+															esc_html( $skill['proficiency'] )
+														);
+													}
+													?>
+												</tbody>
+											</table>
+										</div>
+									</section>
+									<?php do_action( "lpf_{$this->get_post_type()}_after_skills", $applicant, $job ); ?>
+									<section id="languages">
+										<?php
+										$languages       = $applicant->get_languages();
+										$is_multilingual = count( $languages ) > 1;
 
-				?>
-				<h2><?php esc_html_e( 'Languages', 'yikes-level-playing-field' ); ?></h2>
-				<p>
-					<?php
-					echo $is_multilingual ? esc_html__( 'Multilingual', 'yikes-level-playing-field' ) . ' &ndash; ' : '';
-					foreach ( $proficiency_counts as $proficiency => $count ) {
-						echo esc_html( $proficiency_labels[ $proficiency ] ), ' ';
-						echo esc_html( sprintf(
-							/* translators: %d is the number of languages for the given fluency level */
-							_n( 'in %d language', 'in %d languages', $count, 'yikes-level-playing-field' ),
-							$count
-						) );
-						echo $needs_comma ? ', ' : ' ';
-						$needs_comma--;
-					}
-					?>
-				</p>
-			</section>
-			<?php do_action( "lpf_{$this->get_post_type()}_after_languages", $applicant, $job ); ?>
-			<section id="experience">
-				<h2><?php esc_html_e( 'Experience', 'yikes-level-playing-field' ); ?></h2>
-				<ol>
-					<?php
-					foreach ( $applicant->get_experience() as $experience ) {
-						printf(
-							'<li>%s in %s for %s</li>',
-							esc_html( $experience[ ApplicantMeta::POSITION ] ),
-							esc_html( $experience[ ApplicantMeta::INDUSTRY ] ),
-							esc_html( $experience[ ApplicantMeta::YEAR_DURATION ] )
-						);
-					}
-					?>
-				</ol>
-			</section>
-			<?php do_action( "lpf_{$this->get_post_type()}_after_experience", $applicant, $job ); ?>
-			<section id="volunteer-work">
-				<h2><?php esc_html_e( 'Volunteer Work', 'yikes-level-playing-field' ); ?></h2>
-				<ol>
-					<?php
-					foreach ( $applicant->get_volunteer() as $experience ) {
-						printf(
-							'<li>%s in %s for x years</li>',
-							esc_html( $experience['position'] ),
-							esc_html( $experience['industry'] )
-						);
-					}
-					?>
-				</ol>
-			</section>
-			<?php do_action( "lpf_{$this->get_post_type()}_after_volunteer_work", $applicant, $job ); ?>
-			<section id="misc"></section>
-			<?php do_action( "lpf_{$this->get_post_type()}_after_misc", $applicant, $job ); ?>
-			<section id="interview">
-				<h2>Interview Details</h2>
-				<?php if ( $applicant->get_interview_status() === 'scheduled' || $applicant->get_interview_status() === 'confirmed' ) { ?>
-					<?php // @todo: fetch interview data with $applicant->get_interview(). ?>
-					<?php $interview = maybe_unserialize( $applicant->__get( 'interview' ) ); ?>
-					<p><span class="label"><?php esc_html_e( 'Date:', 'yikes-level-playing-field' ); ?></span>
-					<?php echo esc_html( $interview['date'] ); ?></p>
-					<p><span class="label"><?php esc_html_e( 'Time:', 'yikes-level-playing-field' ); ?></span>
-						<?php echo esc_html( $interview['time'] ); ?></p>
-					<p><span class="label"><?php esc_html_e( 'Location:', 'yikes-level-playing-field' ); ?></span>
-						<?php echo esc_html( $interview['location'] ); ?></p>
-					<p><span class="label"><?php esc_html_e( 'Message:', 'yikes-level-playing-field' ); ?></span>
-						<?php echo esc_html( $interview['message'] ); ?></p>
-				<?php } else { ?>
-					<p><span class="label">An interview has not been scheduled yet.</span>	
-				<?php } ?>
-			</section>
-			<?php do_action( "lpf_{$this->get_post_type()}_after_interview", $applicant, $job ); ?>
-		</article>
-		<?php
+										// Build up language proficiency data.
+										$proficiency_labels = [
+											'fluent'       => __( 'Fluent', 'yikes-level-playing-field' ),
+											'professional' => __( 'Working Professional proficiency', 'yikes-level-playing-field' ),
+											'limited'      => __( 'Limited proficiency', 'yikes-level-playing-field' ),
+											'elementary'   => __( 'Elementary proficiency', 'yikes-level-playing-field' ),
+										];
+
+										$proficiency_counts = [];
+										foreach ( $languages as $language ) {
+											if ( ! array_key_exists( $language['proficiency'], $proficiency_counts ) ) {
+												$proficiency_counts[ $language['proficiency'] ] = 1;
+												continue;
+											}
+
+											$proficiency_counts[ $language['proficiency'] ]++;
+										}
+
+										// Set up a counter for when we need to output a comma.
+										$needs_comma = count( $proficiency_counts ) - 1;
+
+										?>
+										<h4 class="yks_mbox_title"><?php esc_html_e( 'Languages', 'yikes-level-playing-field' ); ?></h4>
+										<div class="applicant-skills-container">
+											<p>
+												<?php
+												echo $is_multilingual ? esc_html__( 'Multilingual', 'yikes-level-playing-field' ) . ' &ndash; ' : '';
+												foreach ( $proficiency_counts as $proficiency => $count ) {
+													echo esc_html( $proficiency_labels[ $proficiency ] ), ' ';
+													echo esc_html( sprintf(
+														/* translators: %d is the number of languages for the given fluency level */
+														_n( 'in %d language', 'in %d languages', $count, 'yikes-level-playing-field' ),
+														$count
+													) );
+													echo $needs_comma ? ', ' : ' ';
+													$needs_comma--;
+												}
+												?>
+											</p>
+										</div>
+									</section>
+									<?php do_action( "lpf_{$this->get_post_type()}_after_languages", $applicant, $job ); ?>
+									<section id="experience">
+										<h4 class="yks_mbox_title"><?php esc_html_e( 'Experience', 'yikes-level-playing-field' ); ?></h4>
+										<div class="applicant-skills-container">
+											<ol>
+												<?php
+												foreach ( $applicant->get_experience() as $experience ) {
+													printf(
+														'<li>%s in %s for %s</li>',
+														esc_html( $experience[ ApplicantMeta::POSITION ] ),
+														esc_html( $experience[ ApplicantMeta::INDUSTRY ] ),
+														esc_html( $experience[ ApplicantMeta::YEAR_DURATION ] )
+													);
+												}
+												?>
+											</ol>
+										</div>
+									</section>
+									<?php do_action( "lpf_{$this->get_post_type()}_after_experience", $applicant, $job ); ?>
+									<section id="volunteer-work">
+										<h4 class="yks_mbox_title"><?php esc_html_e( 'Volunteer Work', 'yikes-level-playing-field' ); ?></h4>
+										<div class="applicant-skills-container">
+											<ol>
+												<?php
+												foreach ( $applicant->get_volunteer() as $experience ) {
+													printf(
+														'<li>%s in %s for x years</li>',
+														esc_html( $experience['position'] ),
+														esc_html( $experience['industry'] )
+													);
+												}
+												?>
+											</ol>
+										</div>
+									</section>
+									<?php do_action( "lpf_{$this->get_post_type()}_after_volunteer_work", $applicant, $job ); ?>
+								</div><!-- /yks_mbox -->
+							</div><!-- /inside -->
+						</div><!-- /postbox -->
+					</div><!-- /meta-box-sortables -->
+				</div><!-- /postbox-container-2 -->
+			</div><!-- /metabox-holder columns-2 -->
+			<!-- /post-body -->
+			<br class="clear">
+			</br>
+		</div><!-- /poststuff -->
+	<?php
 	}
 
 	/**
