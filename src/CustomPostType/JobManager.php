de1d0e72e93cd9166a40447751b753bb351fa9cf
Kevin Utz <kevin@yikesinc.com>
2019-02-19 09:21:43 -0500

    A better translation comment for the rewrite slug.

--- src/CustomPostType/JobManager.php
+++ src/CustomPostType/JobManager.php
@@ -91,7 +91,7 @@ class JobManager extends BaseCustomPostType {
 			],
 			'menu_icon'           => 'dashicons-feedback',
 			'rewrite'             => [
-				'slug' => _x( 'lpf-jobs', 'It might look weird but WordPress\' documentation states that rewrite slugs should be translatable.', 'yikes-level-playing-field' ),
+				'slug' => _x( 'lpf-jobs', 'The CPT\'s rewrite slug. Translatable as per WP\'s documentation.', 'yikes-level-playing-field' ),
 			],
 		];
 	}
