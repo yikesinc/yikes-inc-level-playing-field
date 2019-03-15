f4be5bd35f5bf694c0263c927cf6f590aa88dc72
Kevin Utz <kevin@yikesinc.com>
2019-03-15 15:01:55 -0400

    Removing show/hide all messages from JS, HTML markup, and localized PHP strings

--- views/applicant-messaging.php
+++ views/applicant-messaging.php
@@ -32,32 +32,17 @@ $comments = $this->comments;
 			<?php
 		} else {
 
-			$count    = count( $comments );
-			$suppress = $count > 10;
-			$counter  = 1;
-
 			foreach ( $comments as $comment ) {
 
-				if ( 1 === $counter && $suppress ) {
-					?>
-					<h3 id="conversation-show-all"><?php esc_html_e( 'Show All Messages', 'yikes-level-playing-field' ); ?></h3>
-					<?php
-				}
-
-				$classes = [ 'lpf-message' ];
-
-				$classes[] = $suppress && ( $count - $counter >= 10 ) ? 'hidden' : '';
-
+				$classes   = [ 'lpf-message' ];
 				$classes[] = $comment->get_author() === ApplicantMessage::ADMIN_AUTHOR ? 'message-to-applicant' : 'message-from-applicant';
-
-				$classes = array_map( 'sanitize_html_class', $classes );
+				$classes   = array_map( 'sanitize_html_class', $classes );
 				?>
 				<div class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>">
 					<div class="message"><?php echo nl2br( wp_kses( $comment->get_content(), [ 'br' => [] ] ) ); ?></div>
 					<div class="message-timestamp"><?php echo esc_html( $comment->get_formatted_date() ); ?></div>
 				</div>
 				<?php
-				$counter++;
 			}
 		}
 		?>
