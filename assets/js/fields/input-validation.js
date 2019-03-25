6aa58753023f9c182ac2cbfa502ec1f1f1c521e1
Ebonie <ebonie@metalandcoffee.com>
2019-03-25 18:05:47 -0400

    conditional field progress

--- assets/js/fields/input-validation.js
+++ assets/js/fields/input-validation.js
@@ -1,18 +1,6 @@
 // Application Form Validation.
 jQuery( document ).ready( function( $ ) {
 
-	let isError = false;
-	const $submitBtn = $( '.lpf-submit' );
-	const errorPrompt = 'error-prompt';
-	const i18n = $.extend( true, {
-		// These are defaults in case the window object is missing.
-		// See src/Shortcode/Application.php for localized strings.
-		errors: {
-			empty: 'Field cannot be empty.',
-			invalid: '%TYPE% is invalid.'
-		}
-	}, ( window.lpfInputValidation || {} ) );
-
 	/**
 	 * Test a field value against a regular expression.
 	 *
@@ -154,9 +142,6 @@ jQuery( document ).ready( function( $ ) {
 		isError = false;
 	};
 
-	// When 'Submit' button is clicked...
-	$submitBtn.on( 'click', submitValidation );
-
 	/**
 	 * Toggle the end date's required attr and visibility based on the to-the-present field being checked.
 	 *
@@ -176,6 +161,51 @@ jQuery( document ).ready( function( $ ) {
 			endDate.prop( 'required', true );
 		}
 	};
+
+	// Globals.
+	let isError = false;
+	const $submitBtn = $( '.lpf-submit' );
+	const errorPrompt = 'error-prompt';
+	const i18n = $.extend( true, {
+		// These are defaults in case the window object is missing.
+		// See src/Shortcode/Application.php for localized strings.
+		errors: {
+			empty: 'Field cannot be emptytrt.',
+			invalid: '%TYPE% is invalid.'
+		}
+	}, ( window.lpfInputValidation || {} ) );
+
+	const toggleSchoolingFields = ( event ) => {
+		console.log($( event.target ).val());
+		if ( $(this).val() === 'high_school' ) {
+			$(this).parents('.lpf-fieldset-schooling').find( 'input[id*="[major]"], input[id*="[degree]"]' ).each(function() {
+				$(this).parent('.lpf-input-label').hide();
+			});
+		} else {
+			$(this).parents('.lpf-fieldset-schooling').find( 'input[id*="[major]"], input[id*="[degree]"]' ).each(function() {
+				$(this).parent('.lpf-input-label').show();
+			});
+		}
+	};
+
+	/**
+	 * Initialization.
+	 */
+
+	// If schooling type field is 'High School'.
+	const schoolingFields = $( '.lpf-fieldset-schooling select[id*="[type]"]' );
+	schoolingFields.each(function() {
+		if ( $(this).val() === 'high_school' ) {
+			$(this).parents('.lpf-fieldset-schooling').find( 'input[id*="[major]"], input[id*="[degree]"]' ).each(function() {
+				$(this).parent('.lpf-input-label').hide();
+			});
+		}
+	});
+
+	$( 'body' ).on( 'change', '.lpf-fieldset-schooling select[id*="[type]"]', toggleSchoolingFields );
+
+	// When 'Submit' button is clicked...
+	$submitBtn.on( 'click', submitValidation );
 
 	// When a to-the-present checkbox is clicked.
 	$( 'body' ).on( 'click', 'input[type="checkbox"][name$="[present_position]"]', toggleEndDate );
