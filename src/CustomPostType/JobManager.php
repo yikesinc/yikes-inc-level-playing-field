4454ca7a7e4a87c8b7ab44d981faf28b4457112a
Tracy Levesque <tracy@yikesinc.com>
2019-04-03 16:44:25 -0400

    Adding a svg for our custom LPF icon.

--- src/CustomPostType/JobManager.php
+++ src/CustomPostType/JobManager.php
@@ -133,7 +133,7 @@ class JobManager extends BaseCustomPostType {
 				'edit_published_posts'   => Capabilities::EDIT_PUBLISHED_JOBS,
 				'create_posts'           => Capabilities::CREATE_JOBS,
 			],
-			'menu_icon'           => 'dashicons-feedback',
+			'menu_icon'           => 'data:image/svg+xml;base64,' . base64_encode( '<svg width="20" height="20" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path fill="black" d="M1591 1448q56 89 21.5 152.5t-140.5 63.5h-1152q-106 0-140.5-63.5t21.5-152.5l503-793v-399h-64q-26 0-45-19t-19-45 19-45 45-19h512q26 0 45 19t19 45-19 45-45 19h-64v399zm-779-725l-272 429h712l-272-429-20-31v-436h-128v436z"/></svg>' ),
 			'rewrite'             => [
 				'slug' => _x( 'lpf-jobs', "The CPT's rewrite slug. Translatable as per WP's documentation.", 'yikes-level-playing-field' ),
 			],
