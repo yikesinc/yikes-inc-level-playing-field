e0e7369cadea2ee470f90f6cc0e0e5df35f58d05
Tracy Levesque <tracy@yikesinc.com>
2019-04-04 13:58:10 -0400

    Added content to the first banner metabox. Created a Sass partial for banner styles.

--- views/banners.php
+++ views/banners.php
@@ -12,4 +12,24 @@ namespace Yikes\LevelPlayingField;
 ?>
 
 <!-- Banners -->
-<div></div>
+<div class="banners-inner">
+	<p>
+		<?php esc_html_e( 'Leave a review!', 'yikes-level-playing-field' ); ?>
+	</p>
+	<div class="banners-star-container">
+		<a href="https://wordpress.org/support/plugin/level-playing-field/reviews/?rate=5#new-post" target="_blank">
+			<span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span>
+		</a>
+	</div>
+
+	<div class="banners-tweet-container">
+		<p>
+			<?php esc_html_e( 'Tweet about Level Playing Field', 'yikes-level-playing-field' ); ?>
+		</p>
+		<p>
+			<a class="button button-primary banners-tweet-button" href="https://twitter.com/intent/tweet?text=I use Level Playing Field by @yikesinc to receive anonymized job applications to fight bias in hiring and employment. #WordPress &url=https://wordpress.org/plugins/level-playing-field/" target="_blank" data-size="large">
+				<?php esc_html_e( 'Tweet', 'yikes-level-playing-field' ); ?>			
+			</a>
+		</p>
+	</div>
+</div>
