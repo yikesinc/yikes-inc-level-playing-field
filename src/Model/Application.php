3240cca2754ca4a94b767e81dfa1b8fd455340a1
Ebonie <ebonie@metalandcoffee.com>
2019-04-08 12:21:42 -0400

    simplify key name

--- src/Model/Application.php
+++ src/Model/Application.php
@@ -87,7 +87,7 @@ class Application extends CustomPostTypeEntity {
 	 * @return bool
 	 */
 	public function is_required( $field ) {
-		return $this->{AMMeta::REQUIRED}[ $field . AMMeta::REQUIRED_SUFFIX ];
+		return $this->{AMMeta::REQUIRED}[ $field ];
 	}
 
 	/**
@@ -111,14 +111,14 @@ class Application extends CustomPostTypeEntity {
 				: $default;
 
 			// If required flag for current field exists.
-			$this->{AMMeta::REQUIRED}[ $key . AMMeta::REQUIRED_SUFFIX ] = array_key_exists( AMMeta::META_PREFIX . $key . AMMeta::REQUIRED_SUFFIX, $meta );
+			$this->{AMMeta::REQUIRED}[ $key ] = array_key_exists( AMMeta::META_PREFIX . $key . AMMeta::REQUIRED_SUFFIX, $meta );
 		}
 
 		// Name and email fields are always active and required.
 		$this->{AMMeta::NAME}  = true;
 		$this->{AMMeta::EMAIL} = true;
-		$this->{AMMeta::REQUIRED}[ AMMeta::NAME . AMMeta::REQUIRED_SUFFIX ]  = true;
-		$this->{AMMeta::REQUIRED}[ AMMeta::EMAIL . AMMeta::REQUIRED_SUFFIX ] = true;
+		$this->{AMMeta::REQUIRED}[ AMMeta::NAME ]  = true;
+		$this->{AMMeta::REQUIRED}[ AMMeta::EMAIL ] = true;
 	}
 
 	/**
