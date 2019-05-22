4e9afcb6094f693bd191b6688cefc7e829d08143
Kevin Utz <kevin@yikesinc.com>
2019-05-22 11:14:24 -0400

    New info on interview details div for pending interviews

--- views/applicant-interview-details.php
+++ views/applicant-interview-details.php
@@ -20,18 +20,29 @@ $applicant = $this->applicant;
 	<div class="inside">
 		<?php if ( $applicant->get_interview_status() === 'scheduled' || $applicant->get_interview_status() === 'confirmed' ) { ?>
 			<?php $interview = $applicant->get_interview(); ?>
+
+			<?php if ( $applicant->get_interview_status() === 'scheduled' ) { ?>
+				<p>
+					<span class="label"><?php esc_html_e( 'Pending:', 'yikes-level-playing-field' ); ?></span> <?php esc_html_e( 'Awaiting Applicant Confirmation', 'yikes-level-playing-field' ); ?>
+				</p>
+			<?php } ?>
+
 			<p>
 				<span class="label"><?php esc_html_e( 'Date:', 'yikes-level-playing-field' ); ?></span> <?php echo esc_html( $interview['date'] ); ?>
 			</p>
 			<p>
 				<span class="label"><?php esc_html_e( 'Time:', 'yikes-level-playing-field' ); ?></span> <?php echo esc_html( $interview['time'] ); ?>
 			</p>
-			<p>
-				<span class="label"><?php esc_html_e( 'Location:', 'yikes-level-playing-field' ); ?></span> <?php echo esc_html( $interview['location'] ); ?>
-			</p>
-			<p>
-				<span class="label"><?php esc_html_e( 'Message:', 'yikes-level-playing-field' ); ?></span> <?php echo esc_html( $interview['message'] ); ?>
-			</p>
+
+			<?php if ( $applicant->get_interview_status() === 'confirmed' ) { ?>
+				<p>
+					<span class="label"><?php esc_html_e( 'Location:', 'yikes-level-playing-field' ); ?></span> <?php echo esc_html( $interview['location'] ); ?>
+				</p>
+				<p>
+					<span class="label"><?php esc_html_e( 'Message:', 'yikes-level-playing-field' ); ?></span> <?php echo esc_html( $interview['message'] ); ?>
+				</p>
+			<?php } ?>
+
 		<?php } else { ?>
 			<p><span class="label"><?php esc_html_e( 'An interview has not been scheduled yet', 'yikes-level-playing-field' ); ?>.</span>
 		<?php } ?>
