f943d142c5384f42ad05980c0dded017b311c20a
Ebonie Butler <ebonie@metalandcoffee.com>
2019-06-17 14:58:11 -0400

    formatting

--- views/jobs.php
+++ views/jobs.php
@@ -13,8 +13,6 @@ namespace Yikes\LevelPlayingField;
 
 ?>
 <ul class="lpf-jobs-list">
-	<?php
-	echo $this->render_partial( $this->partials['jobs_loop'] );
-	?>
+	<?php echo $this->render_partial( $this->partials['jobs_loop'] ); ?>
 </ul>
 <?php
