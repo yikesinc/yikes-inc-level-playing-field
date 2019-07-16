63d1679aead383fca0f74611848eee26071cefac
freddiemixell <fmixell@gmail.com>
2019-07-16 13:31:32 -0400

    applicant status cancelled

--- views/applicant-interview-details.php
+++ views/applicant-interview-details.php
@@ -12,16 +12,17 @@ namespace Yikes\LevelPlayingField;
 use Yikes\LevelPlayingField\Model\Applicant;
 
 /** @var Applicant $applicant */
-$applicant = $this->applicant;
+$applicant        = $this->applicant;
+$interview_status = $applicant->get_interview_status();
 ?>
 
 <!-- Interview details sidebar -->
 <div id="interview" class="postbox">
 	<div class="inside">
-		<?php if ( $applicant->get_interview_status() === 'scheduled' || $applicant->get_interview_status() === 'confirmed' ) { ?>
+		<?php if ( 'scheduled' === $interview_status || 'confirmed' === $interview_status ) { ?>
 			<?php $interview = $applicant->get_interview(); ?>
 
-			<?php if ( $applicant->get_interview_status() === 'scheduled' ) { ?>
+			<?php if ( 'scheduled' === $interview_status ) { ?>
 				<p>
 					<span class="label"><?php esc_html_e( 'Pending:', 'yikes-level-playing-field' ); ?></span> <?php esc_html_e( 'Awaiting Applicant Confirmation', 'yikes-level-playing-field' ); ?>
 				</p>
@@ -34,7 +35,7 @@ $applicant = $this->applicant;
 				<span class="label"><?php esc_html_e( 'Time:', 'yikes-level-playing-field' ); ?></span> <?php echo esc_html( $interview['time'] ); ?>
 			</p>
 
-			<?php if ( $applicant->get_interview_status() === 'confirmed' ) { ?>
+			<?php if ( 'confirmed' === $interview_status ) { ?>
 				<p>
 					<span class="label"><?php esc_html_e( 'Location:', 'yikes-level-playing-field' ); ?></span> <?php echo esc_html( $interview['location'] ); ?>
 				</p>
@@ -43,7 +44,13 @@ $applicant = $this->applicant;
 				</p>
 			<?php } ?>
 
-		<?php } else { ?>
+		<?php } ?>
+
+		<?php if ( 'cancelled' === $interview_status ) { ?>
+			<p>
+				<?php esc_html_e( 'Interview request cancelled by applicant.', 'yikes-level-playing-field' ); ?>
+			</p>
+        <?php }	else { ?>
 			<p>
 				<?php esc_html_e( 'An interview has not been scheduled.', 'yikes-level-playing-field' ); ?>
 			</p>
