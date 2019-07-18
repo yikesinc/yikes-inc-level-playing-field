2c7bdfb693243fbda003d0791aa697dfece49d14
freddiemixell <fmixell@gmail.com>
2019-07-18 11:24:07 -0400

    removing test

--- views/applicant-interview-details.php
+++ views/applicant-interview-details.php
@@ -16,8 +16,6 @@ $applicant        = $this->applicant;
 $interview_status = $applicant->get_interview_status();
 ?>
 
-<div id="test"></div>
-
 <!-- Interview details sidebar -->
 <div id="interview" class="postbox">
 	<div class="inside">
