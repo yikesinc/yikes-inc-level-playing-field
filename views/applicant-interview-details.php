d7ff0534f4291d2f798185ad57fb1dae500d49b2
freddiemixell <fmixell@gmail.com>
2019-07-19 13:04:34 -0400

    interview details target

--- views/applicant-interview-details.php
+++ views/applicant-interview-details.php
@@ -16,42 +16,7 @@ $applicant        = $this->applicant;
 $interview_status = $applicant->get_interview_status();
 ?>
 
+<div id="test"></div>
+
 <!-- Interview details sidebar -->
-<div id="interview" class="postbox">
-	<div class="inside">
-		<?php if ( 'scheduled' === $interview_status || 'confirmed' === $interview_status ) { ?>
-			<?php $interview = $applicant->get_interview(); ?>
-
-			<?php if ( 'scheduled' === $interview_status ) { ?>
-				<p>
-					<span class="label"><?php esc_html_e( 'Pending:', 'yikes-level-playing-field' ); ?></span> <?php esc_html_e( 'Awaiting Applicant Confirmation', 'yikes-level-playing-field' ); ?>
-				</p>
-			<?php } ?>
-
-			<p>
-				<span class="label"><?php esc_html_e( 'Date:', 'yikes-level-playing-field' ); ?></span> <?php echo esc_html( $interview['date'] ); ?>
-			</p>
-			<p>
-				<span class="label"><?php esc_html_e( 'Time:', 'yikes-level-playing-field' ); ?></span> <?php echo esc_html( $interview['time'] ); ?>
-			</p>
-
-			<?php if ( 'confirmed' === $interview_status ) { ?>
-				<p>
-					<span class="label"><?php esc_html_e( 'Location:', 'yikes-level-playing-field' ); ?></span> <?php echo esc_html( $interview['location'] ); ?>
-				</p>
-				<p>
-					<span class="label"><?php esc_html_e( 'Message:', 'yikes-level-playing-field' ); ?></span> <?php echo esc_html( $interview['message'] ); ?>
-				</p>
-			<?php } ?>
-
-		<?php } elseif ( 'cancelled' === $interview_status ) { ?>
-			<p>
-				<?php esc_html_e( 'Interview request cancelled by the applicant.', 'yikes-level-playing-field' ); ?>
-			</p>
-		<?php } else { ?>
-			<p>
-				<?php esc_html_e( 'An interview has not been scheduled.', 'yikes-level-playing-field' ); ?>
-			</p>
-		<?php } ?>
-	</div>
-</div>
+<div id="interview" class="postbox"></div>
