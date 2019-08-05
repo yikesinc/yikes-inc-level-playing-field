04cdae8c6a0b5dd4612296606103a2de6d430cfa
freddiemixell <fmixell@gmail.com>
2019-08-05 12:14:56 -0400

    API Settings

---
+++ src/REST/APISettings.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * YIKES Inc. Level Playing Field Plugin.
+ *
+ * @package   Yikes\LevelPlayingField
+ * @author    Freddie Mixell
+ * @license   GPL2
+ */
+
+namespace Yikes\LevelPlayingField\REST;
+
+/**
+ * Trait APISettings
+ *
+ * @since   %VERSION%
+ * @package Yikes\LevelPlayingField
+ */
+
+interface APISettings {
+
+	// API SETTINGS.
+	const API_VERSION   = 1;
+	const LPF_NAMESPACE = 'yikes-level-playing-field/v' . self::API_VERSION;
+
+	// API ROUTES.
+	const INTERVIEW_STATUS_ROUTE = '/interview-status/';
+
+}
