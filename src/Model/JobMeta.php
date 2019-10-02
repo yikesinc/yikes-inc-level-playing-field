61bc6386c801d316bd8abc74db2c887e2937a7d0
Ebonie <ebonie@yikesinc.com>
2019-10-02 14:39:28 -0400

    add back pro meta constants

--- src/Model/JobMeta.php
+++ src/Model/JobMeta.php
@@ -29,9 +29,33 @@ interface JobMeta {
 	const APPLICATION_SUCCESS_MESSAGE = 'application_success_message';
 	const APPLICATION_PAGE            = 'application_page';
 
+	// Responsibilities.
+	const RESPONSIBILITIES = 'responsibilities';
+	const SCHEDULE         = 'schedule';
+	const REQUIREMENTS     = 'requirements';
+
+	// Qualifications.
+	const QUALIFICATIONS = 'qualifications';
+	const EDUCATION      = 'education';
+	const EXPERIENCE     = 'experience';
+	const SKILLS         = 'skills';
+
+	// Compensation.
+	const COMPENSATION = 'compensation';
+
+	// Company Details.
+	const NAME     = 'company_name';
+	const DESC     = 'company_desc';
+	const LOGO     = 'company_logo';
+	const WEBSITE  = 'company_website';
+	const TWITTER  = 'company_twitter';
+	const FACEBOOK = 'company_facebook';
+	const LINKEDIN = 'company_linkedin';
+
 	// Properties that should be JSON-encoded.
 	const JSON_PROPERTIES = [
 		self::META_PREFIX . self::ADDRESS => true,
+		self::META_PREFIX . self::LOGO    => true,
 	];
 
 	// Meta prefixed fields.
@@ -42,6 +66,22 @@ interface JobMeta {
 		self::APPLICATION                 => MetaLinks::APPLICATION,
 		self::APPLICATION_SUCCESS_MESSAGE => self::META_PREFIX . self::APPLICATION_SUCCESS_MESSAGE,
 		self::APPLICATION_PAGE            => self::META_PREFIX . self::APPLICATION_PAGE,
+		self::RESPONSIBILITIES            => self::META_PREFIX . self::RESPONSIBILITIES,
+		self::SCHEDULE                    => self::META_PREFIX . self::SCHEDULE,
+		self::REQUIREMENTS                => self::META_PREFIX . self::REQUIREMENTS,
+		self::QUALIFICATIONS              => self::META_PREFIX . self::QUALIFICATIONS,
+		self::EDUCATION                   => self::META_PREFIX . self::EDUCATION,
+		self::EXPERIENCE                  => self::META_PREFIX . self::EXPERIENCE,
+		self::SKILLS                      => self::META_PREFIX . self::SKILLS,
+		self::COMPENSATION                => self::META_PREFIX . self::COMPENSATION,
+		self::NAME                        => self::META_PREFIX . self::NAME,
+		self::DESC                        => self::META_PREFIX . self::DESC,
+		self::LOGO                        => self::META_PREFIX . self::LOGO,
+		self::WEBSITE                     => self::META_PREFIX . self::WEBSITE,
+		self::TWITTER                     => self::META_PREFIX . self::TWITTER,
+		self::FACEBOOK                    => self::META_PREFIX . self::FACEBOOK,
+		self::LINKEDIN                    => self::META_PREFIX . self::LINKEDIN,
+
 	];
 
 	// Fields to expose in REST API.
